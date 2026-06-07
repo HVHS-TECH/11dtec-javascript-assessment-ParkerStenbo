ad26a66d41a9603ec4f43a3cb08f39ec4d910a5e
ParkerStenbo <24378ps@hvhs.school.nz>
2026-06-08 10:08:55 +1200

    I got some way to add buttons to the map with javascript. still buggy

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello world!")
+/*-------------------------------------------------------------PROGRAM START-------------------------------------------------------------*/
+console.log("Running Script.js");
+/*---------------------------------------------------------VARIABLE  DECLARATION---------------------------------------------------------*/
+let locations = ["Hyrule caslte", "Hyrule feild", "Kokiri village", "Field town"];
+let locationX = [0, 10, 20, 30];
+let locationY = [0, 10, 20, 30];
+let locationW = [5, 5, 5, 5];
+let locationH = [5, 5, 5, 5];
+
+const OUTPUT = document.getElementById("buttonsContainer");
+console.log("variables declared successfully");
+/*---------------------------------------------------------------MAIN CODE---------------------------------------------------------------*/
+placeButton(1)
+
+function placeButton(_number) {
+    let tmp= _number -1;
+    OUTPUT.innerHTML = "<button style='margin-left:" + ((50 + locationX[tmp]) - (locationW[tmp]/2)) + "%; margin-top:" + ((50 + locationY[tmp]) - (locationH[tmp]/2)) + "%; height: " + locationH[tmp] + "%;'>" + locations[tmp] + "</button>";
+    console.log(((locationX[tmp]) - locationW[tmp]));
+}
