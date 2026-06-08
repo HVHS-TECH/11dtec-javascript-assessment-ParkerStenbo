32ef5d8d9c31a4d8e21fe21920bef6a357ed1217
ParkerStenbo <24378ps@hvhs.school.nz>
2026-06-08 15:04:34 +1200

    I did something that I think will allow me to have the buttons on at the same time but I'm not sure how easy it will be to calculate the position.

--- script.js
+++ script.js
@@ -2,18 +2,20 @@
 console.log("Running Script.js");
 /*---------------------------------------------------------VARIABLE  DECLARATION---------------------------------------------------------*/
 let locations = ["Hyrule caslte", "Hyrule feild", "Kokiri village", "Field town"];
-let locationX = [0, 10, 20, 30];
+let locationX = [100, 10, 20, 30];
 let locationY = [0, 10, 20, 30];
-let locationW = [5, 5, 5, 5];
-let locationH = [5, 5, 5, 5];
+let locationW = [9, 5, 5, 5];
+let locationH = [12, 5, 5, 5];
 
 const OUTPUT = document.getElementById("buttonsContainer");
 console.log("variables declared successfully");
 /*---------------------------------------------------------------MAIN CODE---------------------------------------------------------------*/
-placeButton(1)
+placeButton(1);
 
 function placeButton(_number) {
     let tmp= _number -1;
-    OUTPUT.innerHTML = "<button style='margin-left:" + ((50 + locationX[tmp]) - (locationW[tmp]/2)) + "%; margin-top:" + ((50 + locationY[tmp]) - (locationH[tmp]/2)) + "%; height: " + locationH[tmp] + "%;'>" + locations[tmp] + "</button>";
-    console.log(((locationX[tmp]) - locationW[tmp]));
+    let x = ((50 + locationX[tmp]) - (locationW[tmp]/2));
+    let y = ((50 + locationY[tmp]) - (locationH[tmp]/2)) * (2/3);
+    OUTPUT.innerHTML += "<button style=' padding:0; border:0; translate:" + x + "%" + y + "%; width: " + locationW[tmp] + "%; height: " + locationH[tmp] + "%;'>" + locations[tmp] + "</button>";
+    console.log(((50+locationY[tmp]) - locationH[tmp]));
 }
