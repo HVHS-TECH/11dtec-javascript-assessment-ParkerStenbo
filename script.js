66c209354ee64cc843d30f47bf47525d8d28d97c
ParkerStenbo <24378ps@hvhs.school.nz>
2026-06-09 10:44:12 +1200

    This will not work imma have to go for a different approach.

--- script.js
+++ script.js
@@ -2,20 +2,30 @@
 console.log("Running Script.js");
 /*---------------------------------------------------------VARIABLE  DECLARATION---------------------------------------------------------*/
 let locations = ["Hyrule caslte", "Hyrule feild", "Kokiri village", "Field town"];
-let locationX = [100, 10, 20, 30];
-let locationY = [0, 10, 20, 30];
+let locationX = [0, 10, 20, 30];
+let locationY = [20, 10, 20, 30];
 let locationW = [9, 5, 5, 5];
 let locationH = [12, 5, 5, 5];
 
 const OUTPUT = document.getElementById("buttonsContainer");
 console.log("variables declared successfully");
 /*---------------------------------------------------------------MAIN CODE---------------------------------------------------------------*/
-placeButton(1);
+console.log(locations.length);
+for (let i = 1; i <= locations.length; i++)
+{
+    OUTPUT.innerHTML += "<button style='width:100%; height:100%; padding:0; margin:1; border:0; margin-top: -135%; background-color:rgb(0, 0, 0);'>";
+    placeButton(i);
+    //OUTPUT.innerHTML += "<button style=' margin-left:-100%; margin-bottom:100%;'>RAAR</button>";
+    console.log("i=" + i);
+    OUTPUT.innerHTML += "</button>";
+}
+
+/*---------------------------------------------------------------FUNCTIONS---------------------------------------------------------------*/
 
 function placeButton(_number) {
     let tmp= _number -1;
     let x = ((50 + locationX[tmp]) - (locationW[tmp]/2));
     let y = ((50 + locationY[tmp]) - (locationH[tmp]/2)) * (2/3);
-    OUTPUT.innerHTML += "<button style=' padding:0; border:0; translate:" + x + "%" + y + "%; width: " + locationW[tmp] + "%; height: " + locationH[tmp] + "%;'>" + locations[tmp] + "</button>";
+    OUTPUT.innerHTML += "<button style='z-index:" + _number * 10 + "; padding:0; border:0; margin-left:" + ((50 + locationX[tmp]) - (locationW[tmp]/2)) + "%; margin-top:" + ((50 + locationY[tmp]) - (locationH[tmp]/2)) * (2/3) + "%; width: " + locationW[tmp] + "%; height: " + locationH[tmp] + "%;'>" + locations[tmp] + "</button>";
     console.log(((50+locationY[tmp]) - locationH[tmp]));
 }
