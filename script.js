7a731ad0c00145e199e3cf6cda6e4ac5bd08e181
ParkerStenbo <24378ps@hvhs.school.nz>
2026-06-16 11:00:37 +1200

    Just fixed some backend stuff making adding locations and changing things around easier

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 console.log("Running Script.js");
 /*---------------------------------------------------------VARIABLE  DECLARATION---------------------------------------------------------*/
 //Location information
-let locations = ["Hyrule caslte", "Hyrule caslte", "Hyrule feild", "Hyrule feild", "Kokiri village", "Field town", "lost woods", "forest village", "forest temple", "forest temple", "tower of spirits", "northern town"];
-let locationX = [-14, -9, -24, -20, -25, -6, -40, -38, -43.5, -45.5, 0.5, -43.5];
-let locationY = [12, -1, 1, -14, 4, -9, -23, -15, -7, -7, 60, 23];
-let locationW = [9, 9, 10, 10, 10, 16, 16, 12, 7, 3, 10, 10];
-let locationH = [20, 7, 25, 10, 10, 20, 16, 10, 7, 9, 45, 15];
-let locationID = [1, 1, 2, 2, 3, 4, 5, 6, 7, 7, 8, 9];
+let locations = ["Hyrule caslte", "Hyrule caslte", "Hyrule feild", "Hyrule feild", "Kokiri village", "Field town", "lost woods", "forest village", "forest temple", "forest temple", "tower of spirits", "northern town", "northern town", "desert town", "desert", "big island", "island village", "harbor town", "isolated island"];
+let locationX = [-14,               -9,              -24,            -20,            -25,              -6,          -40,          -38,              -43.5,          -45.5,            0.5,                -43.5,           -43.5,           7.5,           13,       22,           23.5,             36,            41];
+let locationY = [10.5,              -2,              -6,             -15,            2.5,              -10,         -25,          -17,              -8,             -9,               60,                 23,              20,              -9,            5,        -18,          -8.5,               7,             -23];
+let locationW = [9,                 9,               10,             10,             10,               16,          16,           12,                7,             3,                10,                 7,               10,              7,             14,       20,           6,                17,            14];
+let locationH = [20,                7,               25,             10,             13.5,             20,          16,           10,                7,             9,                45,                 15,              13,              14,       9,            9,               15,            14];
+let locationID = [1,                1,               2,              2,              3,                4,           5,            6,                 7,             7,                8,                  9,               9,               10,            11,       12,           13,               14,            15];
+
+
 //Price is defined by getting the price with locationTravelPrice[locationID[i]]
-let locationTravelPrice = [0, 1, 1, 1, 1, 4, 1, 5, 12, 1];
+let locationTravelPrice = [0,      1,              1,                1,               1,            4,             1,               5,              12,                       1,                 1,             2,       2,            1,                1,             20];
 //so is the displayed location for the recipt
-let locationDisplayName = ["N/A", "Hyrule castle", "Hyrule feild", "Kokiri village", "Field town", "Lost woods", "Forest village", "Forest temple", "The tower of spirits", "Northern village"]
+let locationDisplayName = ["N/A", "Hyrule castle", "Hyrule feild", "Kokiri village", "Field town", "Lost woods", "Forest village", "Forest temple", "The tower of spirits", "Northern village", "Desert Town", "Desert", "Big island", "Island village", "Harbor town", "Isolated island & Ocean temple"];
 
 //temporary list info, picked route
 let pickedRouteLocations = [];
@@ -42,13 +44,13 @@ function refreshBottomBar()
 {
     if (pickedRouteLocations < 1)
     {
-        PREVIEW.innerHTML = "<p> Please choose your first location! </p>";
+        PREVIEW.innerHTML = "<p> Please choose your starting location! </p>";
     }
     else
     {
         PREVIEW.innerHTML = "<p>Your route:</p>";
         listChosenLocations();
-        PREVIEW.innerHTML += "<button class='purchaseButtons' onclick='book()'>Book!!</button> <button class='purchaseButtons' onclick='reset()'>Reset</button>"
+        PREVIEW.innerHTML += "<button class='purchaseButtons' onclick='book()'>Book!!</button> <button class='purchaseButtons' onclick='reset()'>Reset</button> <button class='purchaseButtons' onclick='undoLast()'>UNDO!!</button>"
     }   
 }
 
@@ -73,7 +75,7 @@ function placeButton(_number) {
     let tmp= _number -1;
     let x = ((50 + locationX[tmp]) - (locationW[tmp]/2));
     let y = ((50 + locationY[tmp]) - (locationH[tmp]/2)) * (2/3);
-    OUTPUT.innerHTML += "<button style='background-color: rgba(255, 255, 255, 0.5); position: absolute; padding:0; border:0; margin-left:" + ((50 + locationX[tmp]) - (locationW[tmp]/2)) + "%; margin-top:" + (100-((50 + locationY[tmp]) - (locationH[tmp]/2))) * (2/3) + "%; width: " + locationW[tmp] + "%; height: " + locationH[tmp] + "%;' onclick=' addLocation(" + locationID[tmp] + ") '> </button>";
+    OUTPUT.innerHTML += "<button style='background-color: rgba(255, 255, 255, 0.4); position: absolute; padding:0; border:0; margin-left:" + ((50 + locationX[tmp]) - (locationW[tmp]/2)) + "%; margin-top:" + (100-((50 + locationY[tmp]) - (locationH[tmp]/2))) * (2/3) + "%; width: " + locationW[tmp] + "%; height: " + locationH[tmp] + "%;' onclick=' addLocation(" + locationID[tmp] + ") '> </button>";
     console.log(((50+locationY[tmp]) - locationH[tmp]));
 }
 function addLocation(_id) {
@@ -99,5 +101,20 @@ function reset()
 }
 function book()
 {
-    PREVIEW.innerHTML = "SUCCESSFULLY BOOKED!"
+    if (pickedRouteLocations.length > 1)
+    {
+        PREVIEW.innerHTML = "SUCCESSFULLY BOOKED!"
+    }
+    else
+    {PREVIEW.innerHTML = "<p>Your route:</p>";
+        listChosenLocations();
+        PREVIEW.innerHTML += "<p>Please choose at least 1 destination!</p>"
+        PREVIEW.innerHTML += "<button class='purchaseButtons' onclick='book()'>Book!!</button> <button class='purchaseButtons' onclick='reset()'>Reset</button> <button class='purchaseButtons' onclick='undoLast()'>UNDO!!</button>"
+    }
+}
+function undoLast()
+{
+    pickedRouteLocations.splice((pickedRouteLocations.length-1));
+    pickedRoutePrices.splice(pickedRouteLocations.length);
+    refreshBottomBar();
 }
