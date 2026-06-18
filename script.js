c5e80cbd3bc25795a31c00cbdad1ce75ed19cbe7
ParkerStenbo <24378ps@hvhs.school.nz>
2026-06-18 14:48:23 +1200

    Just continuing to add locations

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 console.log("Running Script.js");
 /*---------------------------------------------------------VARIABLE  DECLARATION---------------------------------------------------------*/
 //Location information
-let locations = ["Hyrule caslte", "Hyrule caslte", "Hyrule feild", "Hyrule feild", "Kokiri village", "Field town", "lost woods", "forest village", "forest temple", "forest temple", "tower of spirits", "northern town", "northern town", "desert town", "desert", "big island", "island village", "harbor town", "isolated island"];
-let locationX = [-14,               -9,              -24,            -20,            -25,              -6,          -40,          -38,              -43.5,          -45.5,            0.5,                -43.5,           -43.5,           7.5,           13,       22,           23.5,             36,            41];
-let locationY = [10.5,              -2,              -6,             -15,            2.5,              -10,         -25,          -17,              -8,             -9,               60,                 23,              20,              -9,            5,        -18,          -8.5,               7,             -23];
-let locationW = [9,                 9,               10,             10,             10,               16,          16,           12,                7,             3,                10,                 7,               10,              7,             14,       20,           6,                17,            14];
-let locationH = [20,                7,               25,             10,             13.5,             20,          16,           10,                7,             9,                45,                 15,              13,              14,       9,            9,               15,            14];
-let locationID = [1,                1,               2,              2,              3,                4,           5,            6,                 7,             7,                8,                  9,               9,               10,            11,       12,           13,               14,            15];
+let locations = ["Hyrule caslte", "Hyrule caslte", "Hyrule feild", "Hyrule feild", "Kokiri village", "Field town", "lost woods", "forest village", "forest temple", "forest temple", "tower of spirits", "northern town", "northern town", "desert town", "desert", "big island", "island village", "harbor town", "isolated island", "Twin tunnels"];
+let locationX = [-14,               -9,              -24,            -20,            -25,              -6,          -40,          -38,              -43.5,          -45.5,            0.5,                -43.5,           -43.5,           7.5,           13,       22,           23.5,             36,            41,                -30];
+let locationY = [10.5,              -2,              -6,             -15,            2.5,              -10,         -25,          -17,              -8,             -9,               60,                 23,              10,              -12,           3,        -18,          -8.5,             7,             -23,                12];
+let locationW = [9,                 9,               10,             10,             10,               16,          16,           12,                7,             3,                10,                 7,               10,              7,             14,       20,           6,                17,            14,                 15];
+let locationH = [20,                7,               25,             10,             13.5,             20,          16,           10,                7,             9,                45,                 15,              7,               13,            14,       9,            9,                15,            14,                 10.5];
+let locationID = [1,                1,               2,              2,              3,                4,           5,            6,                 7,             7,                8,                  9,               9,               10,            11,       12,           13,               14,            15,                 16];
 
 
 //Price is defined by getting the price with locationTravelPrice[locationID[i]]
-let locationTravelPrice = [0,      1,              1,                1,               1,            4,             1,               5,              12,                       1,                 1,             2,       2,            1,                1,             20];
+let locationTravelPrice = [0,      1,              1,                1,               1,            4,             1,               5,              12,                       1,                 1,             2,       2,            1,                1,             20,                                0.5,          ];
 //so is the displayed location for the recipt
-let locationDisplayName = ["N/A", "Hyrule castle", "Hyrule feild", "Kokiri village", "Field town", "Lost woods", "Forest village", "Forest temple", "The tower of spirits", "Northern village", "Desert Town", "Desert", "Big island", "Island village", "Harbor town", "Isolated island & Ocean temple"];
+let locationDisplayName = ["N/A", "Hyrule castle", "Hyrule feild", "Kokiri village", "Field town", "Lost woods", "Forest village", "Forest temple", "The tower of spirits", "Northern village", "Desert Town", "Desert", "Big island", "Island village", "Harbor town", "Isolated island & Ocean temple", "Twin tunnels"];
 
 //temporary list info, picked route
 let pickedRouteLocations = [];
@@ -75,7 +75,7 @@ function placeButton(_number) {
     let tmp= _number -1;
     let x = ((50 + locationX[tmp]) - (locationW[tmp]/2));
     let y = ((50 + locationY[tmp]) - (locationH[tmp]/2)) * (2/3);
-    OUTPUT.innerHTML += "<button style='background-color: rgba(255, 255, 255, 0.4); position: absolute; padding:0; border:0; margin-left:" + ((50 + locationX[tmp]) - (locationW[tmp]/2)) + "%; margin-top:" + (100-((50 + locationY[tmp]) - (locationH[tmp]/2))) * (2/3) + "%; width: " + locationW[tmp] + "%; height: " + locationH[tmp] + "%;' onclick=' addLocation(" + locationID[tmp] + ") '> </button>";
+    OUTPUT.innerHTML += "<button style='background-color: rgba(255, 255, 255, 0); position: absolute; padding:0; border:0; margin-left:" + ((50 + locationX[tmp]) - (locationW[tmp]/2)) + "%; margin-top:" + (100-((50 + locationY[tmp]) - (locationH[tmp]/2))) * (2/3) + "%; width: " + locationW[tmp] + "%; height: " + locationH[tmp] + "%;' onclick=' addLocation(" + locationID[tmp] + ") '> </button>";
     console.log(((50+locationY[tmp]) - locationH[tmp]));
 }
 function addLocation(_id) {
